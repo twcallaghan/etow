45f9461a418df02e84d0f954864e8d7582bcd5c0
Tim Callaghan <tmcallaghan@gmail.com>
2019-02-17 13:24:10 -0800

    game is beta now

--- etow.py
+++ etow.py
@@ -10,6 +10,8 @@ if os.uname()[1] == 'raspberrypi':
 else:
     print('Not running on Raspberry Pi')
 
+p2IsBot=True
+p2Speed=8
 
 # Define the colors we will use in RGB format
 BLACK = (  0,   0,   0)
@@ -30,19 +32,23 @@ done = False
 clock = pygame.time.Clock()
 numTicks=0
 
+numSeconds=0
 p1Mode=0
 p1CountAll=0
 p1Count1s=0
-p1AvgRate=0
 p1Rounds=0
-
 p2Mode=0
 p2CountAll=0
 p2Count1s=0
-p2AvgRate=0
 p2Rounds=0
+p1Speeds={}
+p2Speeds={}
+for x in range(1,11):
+    p1Speeds[x]=0
+    p2Speeds[x]=0
 
-tickSpeed=30
+
+tickSpeed=60
 
 while not done:
     clock.tick(tickSpeed)
@@ -67,7 +73,9 @@ while not done:
         p1Count1s+=1
         p1Mode=0
 
-    if key[K_z] and not key[K_x] and (p2Mode == 0):
+    if (p2IsBot):
+        p2Count1s=p2Speed
+    elif key[K_z] and not key[K_x] and (p2Mode == 0):
         p2CountAll+=1
         p2Count1s+=1
         p2Mode=1
@@ -77,39 +85,50 @@ while not done:
         p2Mode=0
 
     if ((numTicks % tickSpeed) == 0):
-        #p1AvgRate=float(p1CountAll)/float(numTicks)*float(tickSpeed)
-        #p2AvgRate=float(p2CountAll)/float(numTicks)*float(tickSpeed)
-        p1AvgRate=float(p1Count1s)
-        p2AvgRate=float(p2Count1s)
-        print("p1={0:.2f}  p2={1:.2f}".format(p1AvgRate,p2AvgRate))
+        print("p1={0}  p2={1}".format(p1Count1s,p2Count1s))
+        for x in range(1,10):
+            p1Speeds[x]=p1Speeds[x+1]
+            p2Speeds[x]=p2Speeds[x+1]
+        p1Speeds[10]=p1Count1s
+        p2Speeds[10]=p2Count1s
+        if (p1Count1s > p2Count1s):
+            p1Rounds+=1
+        elif (p2Count1s > p1Count1s):
+            p2Rounds+=1
         p1Count1s=0
         p2Count1s=0
-        if (p1AvgRate > p2AvgRate):
-            p1Rounds+=1
-        elif (p2AvgRate > p1AvgRate):
-            p2Rounds+=1
 
-        if (p1Rounds >= 10):
+        if ((p1Rounds-p2Rounds) >= 10):
             print('\nPlayer 1 wins!')
             done=True
-        elif (p2Rounds >= 10):
+        elif ((p2Rounds-p1Rounds) >= 10):
             print('\nPlayer 2 wins!')
             done=True
 
+        numSeconds+=1
+
     screen.fill(WHITE)
 
-    p1Rate=int(round(p1AvgRate))
-    for x in range(0,p1Rate):
-        pygame.draw.rect(screen,BLACK,[screenWidth-100,screenHeight-50-(x*25),25,25])
+    for y in range(1,11):
+        for x in range(0,p1Speeds[y]):
+            pygame.draw.rect(screen,RED,[screenWidth-100-((10-y)*10),screenHeight-50-(x*10),10,10])
 
-    p2Rate=int(round(p2AvgRate))
-    for x in range(0,p2Rate):
-        pygame.draw.rect(screen,BLACK,[100,screenHeight-50-(x*25),25,25])
+    for y in range(1,11):
+        for x in range(0,p2Speeds[y]):
+            pygame.draw.rect(screen,BLUE,[100+(y*10),screenHeight-50-(x*10),10,10])
 
-    for x in range(0,p1Rounds):
-        pygame.draw.rect(screen,BLACK,[screenWidth/2+(x*25),50,25,25])
-
-    for x in range(0,p2Rounds):
-        pygame.draw.rect(screen,BLACK,[screenWidth/2-25-(x*25),50,25,25])
+    knot = p1Rounds - p2Rounds
+    if (p1Rounds > p2Rounds):
+        pygame.draw.rect(screen,RED,[(screenWidth/2)-25+(knot*25),50,50,50])
+    elif (p2Rounds > p1Rounds):
+        pygame.draw.rect(screen,BLUE,[(screenWidth/2)-25+(knot*25),50,50,50])
+    else:
+        pygame.draw.rect(screen,BLACK,[(screenWidth/2)-25+(knot*25),50,50,50])
+    pygame.draw.rect(screen,RED,[(screenWidth/2)-25+(knot*25)+50,70,1000,10])
+    pygame.draw.rect(screen,BLUE,[0,70,(screenWidth/2)-25+(knot*25),10])
 
     pygame.display.flip()
+
+print("Total presses          | p1={0}  p2={1}".format(p1CountAll,p2CountAll))
+print("Avg presses per second | p1={0}  p2={1}".format(p1CountAll/numSeconds,p2CountAll/numSeconds))
+
